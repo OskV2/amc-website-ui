eedf1c7d365fedaad995ae34a39747db39e5c2cd
Maciej Buszkiewicz <maciej.buszkiewicz@o2.pl>
2024-07-01 12:28:55 +0200

    feat: added instagram feed components

---
+++ components/InstagramPost.tsx
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+import pauseIcon from "../public/pause.svg"
+import playIcon from "../public/play.svg"
+
+type InstagramPostType = {
+  id: string;
+  caption: string;
+  media_url: string;
+  permalink: string;
+  media_type: string;
+  thumbnail_url: string;
+  timestamp: Date;
+};
+
+type InstagramPostProps = {
+  postData: InstagramPostType;
+};
+
+const InstagramPost: React.FC<InstagramPostProps> = ({ postData }) => {
+  const [hoverActive, setHoverActive] = useState<boolean>(false)
+
+  const caption = postData.caption.split("#")[0];
+
+  const onMouseOver = (e: React.MouseEvent<HTMLVideoElement>) => {
+    e.target.play()
+    setHoverActive(true)
+  }
+
+  const onMouseOut = (e: React.MouseEvent<HTMLVideoElement>) => {
+    e.target.pause()
+    setHoverActive(false)
+  }
+
+  return (
+    <>
+      <div className="aspect-square overflow-hidden relative">
+        <Link
+          className="inline-block size-full"
+          href={postData.permalink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {(postData.media_type === "IMAGE" ||
+            postData.media_type === "CAROUSEL_ALBUM") && (
+            <div className="inline-block size-full bg-black">
+              <img
+                className="absolute inset-0 my-auto"
+                src={postData.media_url}
+                alt={postData.caption}
+              />
+            </div>
+          )}
+          {postData.media_type === "VIDEO" && (
+            <div className="inline-block size-full bg-black">
+              <Image className="absolute z-10 mt-6 ml-6" src={hoverActive ? playIcon : pauseIcon} alt={`${hoverActive} + icon`} height={20} width={20} />
+              <video
+                className="absolute inset-y-0 my-auto"
+                src={postData.media_url}
+                poster={postData.thumbnail_url}
+                onMouseOver={(e) => onMouseOver(e)}
+                onMouseOut={(e) => onMouseOut(e)}
+                muted
+              />
+            </div>
+          )}
+          <div className="h-10 pl-3 bottom-0 w-full bg-amber-200/25 absolute z-10">
+            <p className="leading-10">{caption}</p>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default InstagramPost;
--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { fetchInstagramPosts } from "../utils/instagram";
+import InstagramPost from "./InstagramPost";
 
-const SocialMedia = () => {
+import instagramIcon from '../public/instagram.svg'
+
+type InstagramPostType = {
+  id: string;
+  caption: string;
+  media_url: string;
+  permalink: string;
+  media_type: string;
+  thumbnail_url: string;
+  timestamp: Date;
+};
+
+const SocialMedia: React.FC = () => {
+  const [posts, setPosts] = useState<InstagramPostType[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const data = await fetchInstagramPosts();
+      setPosts(data);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
-    <div>SocialMedia</div>
-  )
-}
+    <section className="container" id="social">
+      <div className="flex">
+        <h2 className="text-6xl">Sprawdź naszego instagrama</h2>
+        <Image src={instagramIcon} alt="Instagram icon" height={20} width={20} />
+      </div>
+      <div className="grid grid-cols-3 gap-2">
+        {posts.map((post: InstagramPostType) => (
+          <InstagramPost key={post.id} postData={post} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
