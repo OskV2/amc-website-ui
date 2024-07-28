9b54a6cc5fbf5fbf07e611c3af180735ee4474b0
Maciej Buszkiewicz <maciej.buszkiewicz@o2.pl>
2024-07-28 18:46:49 +0200

    feat: added footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import GithubIcon from "../public/github.png";
 
 const Footer = () => {
   return (
-    <footer className='px-24 py-6'>
-      <div className='container'>
-        Footer
+    <footer className="px-24 py-6">
+      <div className="container flex flex-col items-end">
+        <p className="text-white/50 mb-2">Page designed and developed by:</p>
+        <Link
+          className="flex justify-end items-center"
+          href="https://github.com/OskV2"
+          target="_blank"
+        >
+          <Image src={GithubIcon} alt="Github icon" width={24} height={24} />
+          <p className="text-white hover:text-amber-100 ml-2.5">github.com/OskV2</p>
+        </Link>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
