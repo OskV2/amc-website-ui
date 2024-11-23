4ce701b88027bf30f97124035905478b874d1e26
Maciej Buszkiewicz <maciej.buszkiewicz@o2.pl>
2024-11-23 22:45:50 +0100

    feat: minor config changes and project rebuild

--- next.config.js
+++ next.config.js
@@ -1,39 +1,43 @@
 module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'scontent-waw2-2.cdninstagram.com',
-          port: '',
-          pathname: '',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'scontent-waw2-2.cdninstagram.com',
+        port: '',
+        pathname: '/*', // Wildcard to match all paths
+      },
+    ],
+  },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    );
+
+    if (!fileLoaderRule) {
+      throw new Error("Could not find the SVG rule in webpack config");
+    }
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { 
+          not: Array.isArray(fileLoaderRule.resourceQuery?.not)
+            ? [...fileLoaderRule.resourceQuery.not, /url/]
+            : [/url/]
         },
-      ],
-    },
-    webpack(config) {
-      // Grab the existing rule that handles SVG imports
-      const fileLoaderRule = config.module.rules.find((rule) =>
-        rule.test?.test?.('.svg'),
-      )
-  
-      config.module.rules.push(
-        // Reapply the existing rule, but only for svg imports ending in ?url
-        {
-          ...fileLoaderRule,
-          test: /\.svg$/i,
-          resourceQuery: /url/, // *.svg?url
-        },
-        // Convert all other *.svg imports to React components
-        {
-          test: /\.svg$/i,
-          issuer: fileLoaderRule.issuer,
-          resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-          use: ['@svgr/webpack'],
-        },
-      )
-  
-      // Modify the file loader rule to ignore *.svg, since we have it handled now.
-      fileLoaderRule.exclude = /\.svg$/i
-  
-      return config
-    },
-  }
+        use: ['@svgr/webpack'],
+      },
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+};
